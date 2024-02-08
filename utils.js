6891878630b9cb04da6fa1aa0de57f90cfda268d
정혜진 <flfk55@naver.com>
2024-02-08 15:38:42 +0900

    refactor: else 예약어 삭제 및 early return, for 메서드 map, reduce 메서드로 변경, any 타입 수정

--- utils.js
+++ utils.js
@@ -1,26 +1,19 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.cloneDeep = void 0;
-/**깊은 복사를 실행하는 함수 */
-var cloneDeep = function (target) {
-    var result;
-    if (typeof target === "object" && target !== null) {
-        if (Array.isArray(target)) {
-            result = [];
-            for (var i = 0; i < target.length; i++) {
-                result[i] = (0, exports.cloneDeep)(target[i]);
-            }
-        }
-        else {
-            result = {};
-            for (var prop in target) {
-                result[prop] = (0, exports.cloneDeep)(target[prop]);
-            }
-        }
+/** 깊은 복사를 실행하는 함수 */
+const cloneDeep = (target) => {
+    if (typeof target !== "object" || target === null) {
+        return target;
     }
-    else {
-        result = target;
+    if (Array.isArray(target)) {
+        return target.map((item) => (0, exports.cloneDeep)(item));
     }
-    return result;
+    const entries = Object.entries(target);
+    const copy = entries.reduce((acc, [key, value]) => {
+        acc[key] = (0, exports.cloneDeep)(value);
+        return acc;
+    }, {});
+    return copy;
 };
 exports.cloneDeep = cloneDeep;
--- utils.ts
+++ utils.ts
@@ -1,20 +1,19 @@
-/**깊은 복사를 실행하는 함수 */
-export const cloneDeep = <T>(target: T) => {
-  let result: any;
-  if (typeof target === "object" && target !== null) {
-    if (Array.isArray(target)) {
-      result = [];
-      for (let i = 0; i < target.length; i++) {
-        result[i] = cloneDeep(target[i]);
-      }
-    } else {
-      result = {};
-      for (let prop in target) {
-        result[prop] = cloneDeep(target[prop]);
-      }
-    }
-  } else {
-    result = target;
+type KeyValue<T> = [keyof T, T[keyof T]]; // helper type
+
+/** 깊은 복사를 실행하는 함수 */
+export const cloneDeep = <T>(target: T): T => {
+  if (typeof target !== "object" || target === null) {
+    return target;
   }
-  return result;
+
+  if (Array.isArray(target)) {
+    return target.map((item) => cloneDeep(item)) as T;
+  }
+
+  const entries = Object.entries(target) as KeyValue<T>[];
+  const copy = entries.reduce((acc: Partial<T>, [key, value]) => {
+    acc[key] = cloneDeep(value);
+    return acc;
+  }, {});
+  return copy as T;
 };
