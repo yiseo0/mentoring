ba267ff161dd7ba6141aebef3dd88644584f6e9e
flfk55 <flfk55@naver.com>
2024-03-21 01:27:00 +0900

    rename: 폴더 이동

---
+++ week8/kanbanBoard/src/app.js
@@ -0,0 +1,5 @@
+import BoardModel from "./models/BoardModel.js";
+import BoardView from "./views/BoardView.js";
+
+const boardModel = new BoardModel();
+const boardView = new BoardView(boardModel);
--- week8/src/app.js
+++
@@ -1,5 +0,0 @@
-import BoardModel from './models/BoardModel.js'
-import BoardView from './views/BoardView.js'
-
-const boardModel = new BoardModel();
-const boardView = new BoardView(boardModel);
